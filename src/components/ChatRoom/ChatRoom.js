d7987e3199e914912338049aa3c22327b4a77b05
abhinav-22-tech <jainabhinav0860@gmail.com>
2021-11-10 17:20:34 +0530

    room and user style

--- src/components/ChatRoom/ChatRoom.js
+++ src/components/ChatRoom/ChatRoom.js
@@ -3,9 +3,11 @@ import React, { useRef, useState, useEffect } from "react";
 import useChat from "./useChatRoom";
 import "./ChatRoom.css";
 
-const Room = () => {
-  const { messages, sendMessage } = useChat();
-  const [newMessage, setNewMessage] = useState("");
+const Room = (props) => {
+  
+  const { roomId } = props.match.params;
+  const { messages, sendMessage } = useChat(roomId);
+  const [newMessage, setNewMessage] = useState(""); 
   const messageRef = useRef();
 
   const handleNewMessageChange = (event) => {
@@ -21,10 +23,7 @@ const Room = () => {
 
   const handleKeyUp = (event) => {
     if (event.key === "Enter") {
-      if (newMessage !== "") {
-        sendMessage(newMessage);
-        setNewMessage("");
-      }
+      handleSendMessage();
     }
   };
 
@@ -35,7 +34,10 @@ const Room = () => {
       <div>
         <ol id="messages">
           {messages.map((message, i) => (
-            <li key={i}>
+            <li
+              key={i}
+              className={message.isOwner ? "my-message" : "received-message"}
+            >
               <span>{message.body}</span>
             </li>
           ))}
