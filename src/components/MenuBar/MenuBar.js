bdb1f69ee26b024d0faa503660df02f0b9bb1f9b
abhinav-22-tech <jainabhinav0860@gmail.com>
2022-01-04 18:05:44 +0530

    [Layout] Chnage layout of videos

--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -6,25 +6,58 @@ import MicIcon from "@mui/icons-material/Mic";
 import MicOffIcon from "@mui/icons-material/MicOff";
 import VideocamIcon from "@mui/icons-material/Videocam";
 import MessageIcon from "@mui/icons-material/Message";
-import PanToolIcon from '@mui/icons-material/PanTool';
-import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
-import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import CallEndIcon from '@mui/icons-material/CallEnd';
-import { color } from "@mui/system";
+import PanToolIcon from "@mui/icons-material/PanTool";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import CallEndIcon from "@mui/icons-material/CallEnd";
 
 export default function MenuBar() {
   return (
     <>
-      <Box component="span" sx={{ p: 2, backgroundColor: "#141414", ml: 60 }}>
-        <Button color="secondary"><MicIcon/></Button>
-        <Button><VideocamIcon/></Button>
-        <Button><MessageIcon/></Button>
-        <Button><PanToolIcon/></Button>
-        <Button><PeopleAltIcon/></Button>
-        <Button><PersonAddAlt1Icon/></Button>
-        <Button sx={{backgroundColor: "red", color: "white"}}><CallEndIcon/></Button>
-        <Button><MoreHorizIcon/></Button>
+      <Box
+        component="span"
+        sx={{
+          backgroundColor: "#141414",
+          m: "auto",
+          p: 2,
+          borderRadius: 3,
+        }}
+        display="flex" 
+        width={500}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Button>
+          <MicIcon sx={{ color: "#00a389" }} />
+        </Button>
+        <Button sx={{ color: "white" }}>
+          <VideocamIcon />
+        </Button>
+        <Button sx={{ color: "white" }}>
+          <MessageIcon />
+        </Button>
+        <Button sx={{ color: "white" }}>
+          <PanToolIcon />
+        </Button>
+        <Button sx={{ color: "white" }}>
+          <PeopleAltIcon />
+        </Button>
+        <Button sx={{ color: "white" }}>
+          <PersonAddAlt1Icon />
+        </Button>
+        <Button sx={{ color: "white" }}>
+          <MoreHorizIcon />
+        </Button>
+        <Button
+          sx={{
+            backgroundColor: "#fc0303",
+            ":hover": { backgroundColor: "#fa3939" },
+            color: "white",
+          }}
+        >
+          <CallEndIcon />
+        </Button>
       </Box>
     </>
   );
--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -5,6 +5,11 @@ import Participant from "../Participant/Participant";
 import "./Room.css";
 import MenuBar from "../MenuBar/MenuBar";
 
+import { experimentalStyled as styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
 const Room = ({ roomName, room, handleLogout, currentUser }) => {
   const [participants, setParticipants] = useState([]);
   const [displayChat, setDisplayChat] = useState("displayChat");
@@ -40,13 +45,38 @@ const Room = ({ roomName, room, handleLogout, currentUser }) => {
     />
   ));
 
+  const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+
   return (
     <main className="room">
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8 }}>
+          {Array.from(Array(1)).map((_, index) => (
+            <Grid item xs={2} sm={4} md={4} key={index}>
+              {room ? (
+                <Participant
+                  totalParticipant={participants}
+                  key={room.localParticipant.sid}
+                  participant={room.localParticipant}
+                />
+              ) : (
+                ""
+              )}
+              {remoteParticipants}
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
       {/* <TopHeader
         participants={participants}
         participant={room.localParticipant}
       /> */}
-      <div className="all-participants">
+      {/* <div className="all-participants">
         {room ? (
           <Participant
             totalParticipant={participants}
@@ -57,7 +87,7 @@ const Room = ({ roomName, room, handleLogout, currentUser }) => {
           ""
         )}
         {remoteParticipants}
-      </div>
+      </div> */}
       {/* 
       <div className="chatRoom">
         <div className={displayChat}>
