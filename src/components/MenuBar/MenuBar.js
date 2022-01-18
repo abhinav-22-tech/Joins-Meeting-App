6782bce7991ca5e4135a02cce330535ba3c1f183
Abhinav Jain <66718489+abhinav-22-tech@users.noreply.github.com>
2022-01-19 00:10:45 +0530

    Room functions (#13)
    * [Guest] Sigin as guest update
    
    * Audio and Video Toggle Add
    
    * Audio && Video Added

--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -1,10 +1,11 @@
 import React from "react";
-
-import { Button, ButtonGroup } from "@mui/material";
+import { useState, useCallback } from "react";
+import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import MicIcon from "@mui/icons-material/Mic";
 import MicOffIcon from "@mui/icons-material/MicOff";
 import VideocamIcon from "@mui/icons-material/Videocam";
+import VideocamOffIcon from "@mui/icons-material/VideocamOff";
 import MessageIcon from "@mui/icons-material/Message";
 import PanToolIcon from "@mui/icons-material/PanTool";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
@@ -12,7 +13,36 @@ import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import CallEndIcon from "@mui/icons-material/CallEnd";
 
-export default function MenuBar() {
+export default function MenuBar({
+  handleLogout,
+  handleAudioMute,
+  handleAudioUnmute,
+  handleVideoMute,
+  handleVideoUnmute,
+}) {
+  const [audio, setAudio] = useState(true);
+  const [camera, setCamera] = useState(true);
+
+  const audioToggle = () => {
+    if (audio === false) {
+      setAudio(true);
+      handleAudioUnmute();
+    } else if (audio === true) {
+      setAudio(false);
+      handleAudioMute();
+    }
+  };
+
+  const cameraToggle = () => {
+    if (camera === false) {
+      setCamera(true);
+      handleVideoUnmute();
+    } else if (camera === true) {
+      setCamera(false);
+      handleVideoMute();
+    }
+  };
+
   return (
     <>
       <Box
@@ -24,16 +54,24 @@ export default function MenuBar() {
           borderRadius: 3,
           boxShadow: 24,
         }}
-        display="flex" 
+        display="flex"
         width={500}
         alignItems="center"
         justifyContent="center"
       >
-        <Button>
-          <MicIcon sx={{ color: "#00a389" }} />
+        <Button onClick={audioToggle} sx={{ color: "white" }}>
+          {audio === true ? (
+            <MicIcon sx={{ color: "#00a389" }} />
+          ) : (
+            <MicOffIcon sx={{ color: "white" }} />
+          )}
         </Button>
-        <Button sx={{ color: "white" }}>
-          <VideocamIcon />
+        <Button onClick={cameraToggle} sx={{ color: "white" }}>
+          {camera === true ? (
+            <VideocamIcon sx={{ color: "#00a389" }} />
+          ) : (
+            <VideocamOffIcon sx={{ color: "white" }} />
+          )}
         </Button>
         <Button sx={{ color: "white" }}>
           <MessageIcon />
@@ -51,6 +89,7 @@ export default function MenuBar() {
           <MoreHorizIcon />
         </Button>
         <Button
+          onClick={handleLogout}
           sx={{
             backgroundColor: "#fc0303",
             ":hover": { backgroundColor: "#fa3939" },
